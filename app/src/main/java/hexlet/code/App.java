c522c06a458d087bec0e75fcc0af144652e8d984
VOsadchii <909404@gmail.com>
2025-08-04 09:34:08 +0300

    Added picocli parameters and options

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,9 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 @Command(
         name = "gendiff",
@@ -11,14 +13,28 @@ import picocli.CommandLine.Command;
 )
 
 public class App implements Runnable {
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = { "-f", "--format" },
+            description = "output format [default: stylish]",
+            defaultValue = "stylish"
+    )
+    private String format;
+
     public static void main(String[] args) {
         // Важно: используем execute(), а не просто new CommandLine()
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
 
+
     @Override
     public void run() {
-        System.out.println("Hello, World!");
+        System.out.println("Using format: " + format);
     }
 }
