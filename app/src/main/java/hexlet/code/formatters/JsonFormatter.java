f63886753a47975d2fed1859cefbda4d9818bdc9
VOsadchii <909404@gmail.com>
2025-09-21 21:13:51 +0300

    delete unused imports

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -1,14 +1,9 @@
 package hexlet.code.formatters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.core.util.DefaultIndenter;
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import hexlet.code.DiffEntry;
 import hexlet.code.Formatter;
 
-import java.lang.reflect.Field;
 import java.util.List;
 public class JsonFormatter implements Formatter {
     private static final ObjectMapper MAPPER = new ObjectMapper();
